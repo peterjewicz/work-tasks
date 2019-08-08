71a8b4ee0f753c837c781c4647926f00985784c2
peterjewicz <peterjewicz@totalwebconnections.com>
2019-08-08 17:21:49 -0500

    update cal handlers to pull tasks for date

--- src/work_tasks/views/Calendar.cljs
+++ src/work_tasks/views/Calendar.cljs
@@ -14,27 +14,23 @@
 
 (defn update-active-date [newDate currentDate]
   "sets the date passed in to active for display stuff TODO call new tasks for date"
-  (print newDate)
   (reset! currentDate newDate))
 
 (defn render [active tasks]
   (let [calendarDates (calendarHelpers/generate-calendar-display (moment))
         todaysTasks (taskHelpers/get-tasks-due-on-date (.format (moment) "MM/DD/YYYY") tasks)
         currentDate (atom (.format (moment) "MM/DD/YYYY"))]
-    ; (print todaysTasks)
     (fn [active tasks]
-      (print @currentDate)
       [:div..Page.Calendar {:class active}
         [:div.Calendar.header
           [:p {:on-click #(handle-state-change {:type "update-active-view" :value "home"})}"Go Back"]]
         [:div.Calendar.content
           [:h4 "Your Week"]
           [:div.Calendar.calendar
-            (for [date calendarDates]
+            (doall (for [date calendarDates]
               [:p.dateItem {:key (.format date "DD") :class (get-class-for-today @currentDate (.format date "MM/DD/YYYY"))
-                :onClick #(update-active-date (.format date "MM/DD/YYYY") currentDate)} (.format date "DD")])]
+                :onClick #(update-active-date (.format date "MM/DD/YYYY") currentDate)} (.format date "DD")]))]
           [:div.Calendar.todaysTask
             [:h3 "Today's Tasks"]
-              (for [task (taskHelpers/get-tasks-due-on-date (.format (moment) "MM/DD/YYYY") tasks)]
-                (Task task))]
-          [:h2 "I'm the calendar"]]])))
+              (for [task (taskHelpers/get-tasks-due-on-date @currentDate tasks)]
+                (Task task))]]])))
